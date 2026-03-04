c485dbc0c5b8ebe9eaee0e5706526f885c5bbb28
Cecep Nandang <cecep@kima.one>
2026-03-04 15:59:10 +0700

    fix: bug

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -168,7 +168,7 @@ const ProductDetail = () => {
                 </div>
               </div>
 
-              <div className="prose prose-lg max-w-none mb-8">
+              <div className="max-w-none mb-8">
                 <div 
                   className="text-gray-700 leading-relaxed"
                   dangerouslySetInnerHTML={{ __html: product.description }} 
