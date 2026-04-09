c3db437a52f4e69fd651f65abb61a94333a81d2a
Rispan <rispanrobidigjaya0@gmail.com>
2026-04-09 14:16:26 +0700

    fix: bug

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -4,10 +4,44 @@ import WhatsAppButton from '../Common/WhatsAppButton';
 import InstagramButton from '../Common/InstagramButton';
 import ShopeeButton from '../Common/ShopeeButton';
 import { Outlet } from 'react-router-dom';
+import { useEffect } from 'react';
 
 const Layout = ({ children }) => {
+  useEffect(() => {
+    if (window.fbq) return;
+
+    !(function (f, b, e, v, n, t, s) {
+      if (f.fbq) return;
+      n = f.fbq = function () {
+        n.callMethod ? n.callMethod.apply(n, arguments) : n.queue.push(arguments);
+      };
+      if (!f._fbq) f._fbq = n;
+      n.push = n;
+      n.loaded = true;
+      n.version = '2.0';
+      n.queue = [];
+      t = b.createElement(e);
+      t.async = true;
+      t.src = v;
+      s = b.getElementsByTagName(e)[0];
+      s.parentNode.insertBefore(t, s);
+    })(window, document, 'script', 'https://connect.facebook.net/en_US/fbevents.js');
+
+    window.fbq('init', '1247786203864821');
+    window.fbq('track', 'PageView');
+  }, []);
+
   return (
     <div className="min-h-screen flex flex-col">
+      <noscript>
+        <img
+          alt=""
+          height="1"
+          width="1"
+          style={{ display: 'none' }}
+          src="https://www.facebook.com/tr?id=1247786203864821&ev=PageView&noscript=1"
+        />
+      </noscript>
       <Header />
       <main className="flex-grow">
         {children || <Outlet />}
